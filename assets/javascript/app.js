3307a936a9a907f7d01d6d0ffa66c464ba330036
ginnac <ginnacampbell1990@gmail.com>
2019-06-24 21:23:24 -0600

    api giant bomb, cors bypass

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,23 @@
-//  API url = "https://api-v3.igdb.com/games/ + 3b6da92942889b613709def422db975a (API key) + parameters
+
+//URL: https://www.giantbomb.com/api/accessory/[guid]/?api_key=[YOUR API KEY]
+
+var apiKey = "bf4a00432b31ea4966819b748105a4d93da12821";
+
+
+$("#search-button").on("click", function () {
+    //prevent page to refresh
+    event.preventDefault();
+
+    var gameName=$("#game-search-box").val();
+    var queryURL = "https://www.giantbomb.com/api/games/?format=JSON&filter=name:"+ gameName +"&api_key="+ apiKey;
+
+    
+
+    $.ajax({
+        url: "https://bypasscors.herokuapp.com/api/?url="+ encodeURIComponent(queryURL),
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
+
+});
