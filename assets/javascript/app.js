71f89b1dfd07506c57fbaec26181e1ecb5c35655
Tyler Nichols <tegulator98@gmail.com>
2019-06-26 16:20:21 -0600

    changed the API example to giantbomb example URL

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,10 @@
-//  API url = "https://api-v3.igdb.com/games/ + 3b6da92942889b613709def422db975a (API key) + parameters
+//  API example url Giantbomb = "https://www.giantbomb.com/api/games/ + ?api_key=bf4a00432b31ea4966819b748105a4d93da12821 (apikey) + &limit=50&filter=name:halo (parameters)"
+$("#search-button").on("click",function(event){
+    event.preventDefault();
+$.ajax({
+    url: "https://www.giantbomb.com/api/games/?api_key=bf4a00432b31ea4966819b748105a4d93da12821&limit=50&filter=name:halo",
+    method: "GET"
+}).then(function(response){
+    console.log(response)
+})
+});
